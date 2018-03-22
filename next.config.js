4af8924958d7999a7fd96f5485ab39af2f176cea
Daryl Browne <daryl.browne@googlemail.com>
2018-03-22 15:29:07 +0000

    chore(nextScss): enable cssmodules

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withSass = require('@zeit/next-sass')
 module.exports = withSass({
+  cssModules: true,
 	sassLoaderOptions: {
-    cssModules: true,
     includePaths: ["node_modules"]
   }
 })
