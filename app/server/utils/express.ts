69293a0ead2a9871ea4fac6c72cd575f27986333
Daryl Browne <daryl.browne@googlemail.com>
2018-03-24 18:44:55 +0000

    chore(): middleware for allowing xhr requests only

--- app/server/utils/express.ts
+++ app/server/utils/express.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { Server } from 'next';
+import { NextAppRequest } from '../types';
+import { INSPECT_MAX_BYTES } from 'buffer';
 
 export function withRenderer(controller, app: Server, route: string) {
 	return (req, res, next) => controller(req, res, next, app, route);
@@ -17,4 +19,17 @@ export function verifyUser(req: Request, res: Response, next: NextFunction) {
 	}
 
 	res.redirect('/login');
+}
+
+export function xhrOnly(req: NextAppRequest, res: Response, next) {
+	if (req.xhr) {
+		return next();
+	}
+
+	return req.nextAppRenderer.renderError(
+		Error('This is route is only accessible via XHR request'),
+		req,
+		res,
+		req.originalUrl,
+	);
 }
