52f14be0d7f72ecdcd401f802fa6665e5c8aaa4c
Daryl Browne <daryl.browne@googlemail.com>
2018-03-25 08:28:20 +0100

    refactor(attendeesPage): display attendees with generic component

--- app/client/pages/attendees.tsx
+++ app/client/pages/attendees.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import Link from 'next/link';
-import Router from 'next/router';
 import { withAdmin } from '../components/adminLayout';
 import { deleteAttendees } from '../api/attendee';
 import { IAttendee } from '../../server/types/models';
-import withModal from '../components/withModal';
+import CheckboxTable from '../components/CheckboxTable';
 
 class Attendees extends React.Component<{ attendees: IAttendee[] }> {
 	static getInitialProps = async ({ res }) => {
@@ -16,50 +15,19 @@ class Attendees extends React.Component<{ attendees: IAttendee[] }> {
 		};
 	}
 
-	state = {
-		bulkMode: false,
-		selected: {},
-	};
-
-	onCheckboxClick(e: { target: { checked: boolean }}, attendeeId: IAttendee['id']) {
-		const { checked } = e.target;
-		const newSelectedState = {
-			...this.state.selected,
-			[attendeeId]: checked,
-		};
-
-		const newState = {
-			selected: newSelectedState,
-		};
-
-		this.setState(newState);
+	renderHeader() {
+		return (
+			<tr>
+				<th>First name</th>
+				<th>Last name</th>
+				<th className="uk-expand">Email</th>
+				<th className="uk-shrink" />
+				<th className="uk-shrink" />
+			</tr>
+		);
 	}
 
-	componentDidUpdate(_, {selected}) {
-		const bulkMode = Object.keys(this.state.selected).some(attendeeId => {
-			return !!this.state.selected[attendeeId];
-		});
-
-		if (this.state.bulkMode !== bulkMode) {
-			this.setState({ bulkMode });
-		}
-	}
-
-	confirmDelete = () => {
-		const selectedIdsForDeletion = Object.keys(this.state.selected).filter(attendeeId => {
-			return this.state.selected[attendeeId];
-		});
-
-		this.props.showConfirmModal({
-			title: 'Are you sure',
-			body: 'This operation is irreversable and cannot be undone. Are you sure you would like to delete the selected attendees?',
-		})
-		.then(() => deleteAttendees(selectedIdsForDeletion))
-		.then(() => Router.push('/admin/attendees'))
-		.catch(() => undefined);
-	}
-
-	renderRow = (attendee) => {
+	renderRow(attendee, onCheckboxTick, itemIsChecked) {
 		return (
 			<tr key={`attendee-row-${attendee.id}`}>
 				<td className="uk-width-1-4">{attendee.firstName}</td>
@@ -74,78 +42,29 @@ class Attendees extends React.Component<{ attendees: IAttendee[] }> {
 					<input
 						type="checkbox"
 						className="uk-checkbox"
-						checked={this.state.selected[attendee.id] || false}
-						onChange={(e) => this.onCheckboxClick(e, attendee.id)}
+						checked={itemIsChecked}
+						onChange={(e) => onCheckboxTick(e)}
 					/>
 				</td>
 			</tr>
 		);
 	}
 
-	bulkModeButtons() {
-		return (
-			<React.Fragment>
-				<button onClick={this.exitBulkMode} className="uk-button-small uk-button uk-button-default">Cancel</button>
-					<button
-						className="uk-button-small uk-button uk-button-danger uk-margin-left"
-						onClick={this.confirmDelete}
-					>
-						Delete
-					</button>
-			</React.Fragment>
-		);
-	}
-
-	selectAll = () => {
-		const attendeeCheckMap = this.props.attendees.reduce((accum, {id}) => {
-			return {
-				...accum,
-				[id]: true,
-			};
-		}, {});
-		this.setState({ selected: attendeeCheckMap });
-	}
-
-	exitBulkMode = () => {
-		this.setState({ selected: {} });
-	}
-
 	render() {
 		return (
-			<div>
-				<div className="uk-clearfix uk-margin">
+			<CheckboxTable
+				data={this.props.attendees}
+				renderHeaderRow={this.renderHeader}
+				renderRow={this.renderRow}
+				onDelete={(ids) => deleteAttendees(ids)}
+				buttons={(
 					<Link prefetch={true} href="/attendeeCreate" as="/admin/attendees/new">
 						<button className="uk-button-small uk-float-left uk-button uk-button-primary">Add</button>
 					</Link>
-					<div className="uk-float-right">
-						{this.state.bulkMode && this.bulkModeButtons()}
-						<button
-							onClick={this.selectAll}
-							className="uk-button-small uk-button uk-button-text uk-margin-left"
-						>
-							Select all
-						</button>
-					</div>
-				</div>
-				<div className="uk-overflow-auto">
-					<table className="uk-table uk-table-justify uk-table-divider">
-						<thead>
-							<tr>
-								<th className="uk-width-1-4">First name</th>
-								<th className="uk-width-1-4">Last name</th>
-								<th className="uk-width-2-4">Email</th>
-								<th className="uk-shrink" />
-								<th className="uk-shrink" />
-							</tr>
-						</thead>
-						<tbody>
-							{this.props.attendees.map(this.renderRow)}
-						</tbody>
-					</table>
-				</div>
-			</div>
+				)}
+			/>
 		);
 	}
 }
 
-export default withModal(withAdmin({ title: 'Attendees'}, Attendees));
+export default withAdmin({ title: 'Attendees'}, Attendees);
