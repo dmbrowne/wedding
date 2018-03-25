559c32e4e00d433a4fc973e9a9ae662f94b2d244
Daryl Browne <daryl.browne@googlemail.com>
2018-03-25 08:27:24 +0100

    chore(withModal): properly typed

--- app/client/components/withModal.tsx
+++ app/client/components/withModal.tsx
@@ -1,66 +1,94 @@
 import * as React from 'react';
 
+interface ConfirmModalProps {
+	title?: string;
+	body?: string;
+	primaryText?: string;
+	secondaryText?: string;
+}
+
+interface ConfirmModalContent extends ConfirmModalProps {
+	success?: () => any;
+	cancel?: () => any;
+}
+
+interface State {
+	confirmModalContent?: ConfirmModalContent;
+}
+
+export interface ChildProps {
+	showConfirmModal: (options: ConfirmModalProps) => Promise<any>;
+}
+
 export default function modalHOC(Component) {
-	return class ModalRootComponent extends React.Component {
+	return class ModalRootComponent extends React.Component<{}, State> {
 		static async getInitialProps(props) {
-			return (Component.getInitialProps ?
-				Component.getInitialProps(props) :
-				{}
-			)
+			return Component.getInitialProps ? Component.getInitialProps(props) : {};
 		}
 
+		UIkit = undefined;
 		confirmModal = null;
 
-		state = {
-			confirmModalContent: {},
-		};
+		state: State = {};
 
-		showConfirmModal = (options) => {
+		showConfirmModal = (options: ConfirmModalProps) => {
 			return new Promise((resolve, reject) => {
-				const modal = window.UIkit.modal(this.confirmModal)
+				const modal = this.UIkit.modal(this.confirmModal);
 				this.setState({
 					confirmModalContent: {
-						title: options.title,
-						body: options.body,
-						primaryText: options.primaryText,
-						secondaryText: options.secondaryText,
-						success: () => (modal.hide(), resolve()),
-						cancel: () => (modal.hide(), reject()),
-					}
-				}, () => {
-					modal.show()
-				})
-			})
+						...options,
+						success: () => resolve(modal.hide()),
+						cancel: () => reject(modal.hide()),
+					},
+				},
+				() => modal.show());
+			});
 		}
 
+		componentDidMount() {
+			this.UIkit = require('uikit');
+		}
+
+		renderModal(content) {
+			const {
+				title,
+				body,
+				cancel,
+				secondaryText = 'No',
+				success,
+				primaryText = 'Yes',
+			} = content;
+			return (
+				<div className="uk-modal-dialog">
+					<div className="uk-modal-header">
+						{title && <h2 className="uk-modal-title">{title}</h2>}
+					</div>
+					{body && <div className="uk-modal-body">{body}</div>}
+					<div className="uk-modal-footer uk-text-right">
+						<button
+							onClick={cancel ? cancel : null}
+							className="uk-button uk-button-default"
+						>
+							{secondaryText}
+						</button>
+						<button
+							onClick={success ? success : null}
+							className="uk-button uk-button-primary uk-margin-left"
+						>
+							{primaryText}
+						</button>
+					</div>
+				</div>
+			);
+		}
 		render() {
-			const { confirmModalContent } = this.state
+			const { confirmModalContent } = this.state;
+
 			return (
 				<div>
 					<Component {...this.props} showConfirmModal={this.showConfirmModal} />
 					<div className="uk-modal" ref={ref => this.confirmModal = ref}>
-						<div className="uk-modal-dialog">
-							<div className="uk-modal-header">
-								<h2 className="uk-modal-title">{confirmModalContent.title}</h2>
-							</div>
-							<div className="uk-modal-body">{confirmModalContent.body}</div>
-							<div className="uk-modal-footer uk-text-right">
-								<button
-									onClick={confirmModalContent.cancel}
-									className="uk-button uk-button-default"
-									type="button"
-								>
-									{confirmModalContent.secondaryText || 'No'}
-								</button>
-								<button
-									onClick={confirmModalContent.success}
-									className="uk-button uk-button-primary uk-margin-left"
-									type="button"
-								>
-									{confirmModalContent.secondaryText || 'Yes'}
-								</button>
-							</div>
-						</div>
+						{confirmModalContent && this.renderModal(confirmModalContent)}
 					</div>
 				</div>
 			);
