b9f8330857736d19134295c21d5f2539c91dd055
Daryl Browne <daryl.browne@googlemail.com>
2018-03-25 00:13:31 +0000

    create clientSide api for attendee, event and sendGroup CRUD operations

---
+++ app/client/api/sendGroup.ts
@@ -0,0 +1,57 @@
+import { restfulRequest } from './utils';
+import { IAttendee, ISendGroup } from '../../server/types/models';
+
+export function getSendGroups(id?: string) {
+	return restfulRequest({
+		route: id ? `/admin/sendgroup/${id}` : '/admin/sendgroup',
+	});
+}
+
+function sendGroup(
+	groupDetails: Pick<ISendGroup, 'name'> | null,
+	attendeeIds?: Array<IAttendee['id']>,
+	groupId?: string,
+) {
+	return restfulRequest({
+		route: groupId ? `/admin/sendgroup/${groupId}` : '/admin/sendgroup',
+		method: groupId ? 'PUT' : 'POST',
+		body: JSON.stringify({
+			...groupDetails,
+			attendeeIds,
+		}),
+	});
+}
+
+export function createSendGroup(
+	groupDetails: Pick<ISendGroup, 'name'> | null,
+	attendeeIds?: Array<IAttendee['id']>,
+) {
+	return sendGroup(groupDetails, attendeeIds);
+}
+
+export function editSendGroup(
+	groupId: string,
+	groupDetails: Pick<ISendGroup, 'name'> | null,
+	attendeeIds?: Array<IAttendee['id']>,
+) {
+	return sendGroup(groupDetails, attendeeIds, groupId);
+}
+
+export function deleteSendGroup(groupIds: string | string[]) {
+	const multipleDeletion = Array.isArray(groupIds);
+	const route = (multipleDeletion ?
+		'/admin/sendgroup' :
+		`/admin/sendgroup/${groupIds}`
+	);
+
+	const body = (multipleDeletion ?
+		JSON.stringify({ sendGroupIds: groupIds }) :
+		undefined
+	);
+
+	return restfulRequest({
+		route,
+		method: 'DELETE',
+		body,
+	});
+}
