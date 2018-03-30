71f3846e271e1b75e207b3899583b2a8d7c358bd
Daryl Browne <daryl.browne@googlemail.com>
2018-03-30 12:13:42 +0100

    spike(): send emails using mailgun

--- app/server/controllers/emailController.ts
+++ app/server/controllers/emailController.ts
@@ -7,19 +7,34 @@ const mg = mailgun({ username: 'api', apiKey, publicKey, domain: 'thebrownes.inf
 export function sendMail(req, res) {
 	const data = {
 		from: 'The Brownes <y-and-d@thebrownes.info>',
-		to: 'daryl.browne@hotmail.com,',
+		to: 'daryl.browne@hotmail.com, yasminobosi@yahoo.co.uk, daryl.browne@gmail.com',
 		subject: 'Our special day',
-		text: "Thank you for confirming attendence, we look forward to seeing you on the big day",
+		html: '<p>Hello Daryl,</p>\
+		<p>As you know Yasmin and I will wed this year, October. You should have received a \
+		link to our save the date previously, hopefully you did just that!</p>\
+		<br/>\
+		<p>Please do come and join us on our special day. A link to your invite is below.</p>\
+		<p><a href="http://thebrownes.info/" alt="Daryls Invitation">Click here</a> for your invite</p>\
+		<p>Please complete the RSVP at the bottom of the invite by the <strong>31st July</strong>.\
+		unfortunately RSVP\'s completed after this date will not be considered and your place \
+		<i>may</i> be given to someone else</p>\
+		<br />\
+		<p><i>Thank you once again, and hope to see you very soon!! :D</i></p>\
+		<br/>\
+		<br/>\
+		Kind regards,<br/>\
+		The future Mr. and Mrs. Browne of twenty eighteen',
 	};
 
-	mg.messages().send(data, function (error, body) {
-		if (error) {
-			const { name, message } = error;
-			return res.status(500).send({ name, message })
-		}
-		res.send(body)
-		console.log(body);
-	});
+	res.send('ok')
+	// mg.messages().send(data, function (error, body) {
+	// 	if (error) {
+	// 		const { name, message } = error;
+	// 		return res.status(500).send({ name, message });
+	// 	}
+	// 	res.send(body)
+	// 	console.log(body);
+	// });
 
 
 }
