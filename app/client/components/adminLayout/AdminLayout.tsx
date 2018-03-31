f3cadaa2f97eaf094ded586f076aa8df7e245806
Daryl Browne <daryl.browne@googlemail.com>
2018-03-31 17:00:05 +0100

    remove padding from main content in AdminLayout

--- app/client/components/adminLayout/AdminLayout.tsx
+++ app/client/components/adminLayout/AdminLayout.tsx
@@ -52,7 +52,7 @@ export default class AdminLayout extends React.Component<Props> {
 		return (
 			<div className="uk-offcanvas-content">
 				<Titlebar onMenuPress={this.toggleMenu}>{this.props.title}</Titlebar>
-				<main className="uk-container" style={{padding: 15}}>
+				<main style={{paddingTop: 15}}>
 					{this.props.children}
 				</main>
 				<aside ref={ref => this.offMenuEl = ref}>
