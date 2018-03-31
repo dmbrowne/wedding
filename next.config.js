fd8cc3b5060ddb3730b952bae5c50d3f0ee0b491
Daryl Browne <daryl.browne@googlemail.com>
2018-03-31 23:55:44 +0100

    fix(): ability to use both css and scss

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 const withSass = require('@zeit/next-sass')
 const withCss = require('@zeit/next-css')
 const withTypescript = require('@zeit/next-typescript')
+const commonsChunkConfig = require('@zeit/next-css/commons-chunk-config')
+
 module.exports = withTypescript(
   withCss(
     withSass({
+      webpack: (config, { isServer }) => {
+        if (!isServer) {
+          config = commonsChunkConfig(config, /\.(sass|scss|css)$/)
+        }
+        return config
+      },
       pageExtensions: ['js'],
       cssModules: false,
       sassLoaderOptions: {
