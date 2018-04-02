204b948a78c063fad06d23fda3071339d3d34092
Daryl Browne <daryl.browne@googlemail.com>
2018-04-02 21:21:33 +0100

    add config file for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+    {
+      name: 'Wedding',
+      script: './index.js',
+      env: {
+        PORT: 4000,
+        NODE_ENV: 'development',
+      },
+      env_production : {
+        PORT: 4000,
+        NODE_ENV: 'production',
+      }
+    },
+  ],
+};
