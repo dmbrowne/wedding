1526889e94561f1af6f46fb087184689b79d4deb
Daryl Browne <daryl.browne@googlemail.com>
2018-04-06 02:33:32 +0100

    add pacejs

--- app/client/pages/_document.tsx
+++ app/client/pages/_document.tsx
@@ -7,7 +7,9 @@ export default class MyDocument extends Document {
 			<html>
 				<Head>
 					<title key="document-title">Mr and Mrs Browne 2018</title>
+					<script src="/assets/pace.min.js" />
 					<link rel="stylesheet" href="/static/style.css" key="base-styleshet"/>
+					<link rel="stylesheet" href="/assets/pace-theme-flash.css" key="pace-styleshet"/>
 				</Head>
 				<body>
 					<Main />
