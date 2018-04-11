6e7b068f6e8c9bf734d386e18efe8b20ff4ddcab
Daryl Browne <daryl.browne@googlemail.com>
2018-04-11 19:56:40 +0100

    fix(): multer returns 302 if used with verifyUser middleware

--- app/server/routers/uploadRoutes.ts
+++ app/server/routers/uploadRoutes.ts
@@ -1,13 +1,10 @@
 import { Router } from 'express';
 import * as path from 'path';
-import { verifyUser } from '../utils/express';
 import * as multer from 'multer';
 
 const upload = multer({ dest: path.join(__dirname, '../../../uploads/') });
 
 const router = Router();
-
-router.use(verifyUser);
 
 router.post('/', upload.single('image'), (req, res) => {
 	const { file } = req;
