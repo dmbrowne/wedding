f5f3a5de3499fe8ee021d77a5dfd8fab521496b6
Daryl Browne <daryl.browne@googlemail.com>
2018-04-11 19:57:10 +0100

    created route for uploading an image to cloudinary

--- app/server/routers/uploadRoutes.ts
+++ app/server/routers/uploadRoutes.ts
@@ -1,17 +1,20 @@
 import { Router } from 'express';
 import * as path from 'path';
 import * as multer from 'multer';
+import dbModels from '../models';
 
 const upload = multer({ dest: path.join(__dirname, '../../../uploads/') });
 
 const router = Router();
 
-router.post('/', upload.single('image'), (req, res) => {
-	const { file } = req;
-	console.log('file ===============================');
-	console.log(file);
-	console.log('file ===============================');
-	res.send({ success: 'ok'});
+router.post('/', upload.single('image'), (req, res, next) => {
+	dbModels.GalleryImage.addNewImage(req.file.path)
+		.then((result) => {
+			res.send({ result });
+		})
+		.catch(e => {
+			next(e);
+		});
 });
 
 export default router;
