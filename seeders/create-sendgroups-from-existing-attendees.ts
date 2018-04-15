8b7964331be2f13a5db414e65f3b9e8d4124747f
Daryl Browne <daryl.browne@googlemail.com>
2018-04-15 20:37:14 +0100

    seeder for creating sendGroups

--- seeders/create-sendgroups-from-existing-attendees.ts
+++ seeders/create-sendgroups-from-existing-attendees.ts
@@ -1,8 +1,7 @@
 import models from '../app/server/models';
 import * as faker from 'faker';
-import * as fs from 'fs';
+// import * as fs from 'fs';
 import * as path from 'path';
-import attendees from './seededUsers.json';
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
@@ -10,33 +9,31 @@ function getRandomInt(max) {
 
 module.exports = {
 	up: () => {
-		const fakeUsers = attendees.fakeUsers;
-		const sendGroups = [];
-		for (let i = 0; i < 15; i++) {
-			const randomNum1 = getRandomInt(fakeUsers.length - 1);
-			const randomUser1 = fakeUsers.splice(randomNum1, 1);
-			const randomNum2 = getRandomInt(fakeUsers.length - 1);
-			const randomUser2 = fakeUsers.splice(randomNum2, 1);
-			sendGroups.push({
-				name: faker.random.word,
-				email: faker.internet.email(),
-				attendeeIds: [randomUser1.id, randomUser2.id],
+		return models.Attendee.findAll({ raw: true })
+			.then(attendees => {
+				const sendGroups = [];
+				for (let i = 0; i < 15; i++) {
+					const randomNum1 = getRandomInt(attendees.length - 1);
+					const randomUser1 = attendees.splice(randomNum1, 1);
+					const randomNum2 = getRandomInt(attendees.length - 1);
+					const randomUser2 = attendees.splice(randomNum2, 1);
+					sendGroups.push({
+						name: faker.random.word(),
+						email: faker.internet.email(),
+						attendeeIds: [randomUser1.id, randomUser2.id],
+					});
+				}
+				return models.SendGroup.bulkCreate(sendGroups);
 			});
-		}
-		fs.writeFileSync(path.join(__dirname, './seededSendGroups.json'), JSON.stringify({
-			sendGroups,
-		}));
-
-		return models.SendGroup.bulkCreate(sendGroups);
 	},
 
 	down: () => {
-		const seededSendGroups = require(path.join(__dirname, './seededSendGroups.json'));
-		const sendGroupdIds = seededSendGroups.sendGroups.map(sendGroup => sendGroup.id);
-		return models.SendGroup.destroy({
-			where: {
-				id: sendGroupdIds,
-			},
-		});
+		// const seededSendGroups = require(path.join(__dirname, './seededSendGroups.json'));
+		// const sendGroupdIds = seededSendGroups.sendGroups.map(sendGroup => sendGroup.id);
+		// return models.SendGroup.destroy({
+		// 	where: {
+		// 		id: sendGroupdIds,
+		// 	},
+		// });
 	},
 };
