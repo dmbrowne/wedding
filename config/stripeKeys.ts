551f07e38e79640b7ed16c828b95b6c48b007fbf
Daryl Browne <daryl.browne@googlemail.com>
2018-04-15 20:12:43 +0100

    update stripeApi keys skeleton

--- config/stripeKeys.ts
+++ config/stripeKeys.ts
@@ -1,7 +1,13 @@
-export const publicKey = '';
-export const secretKey = '';
+export const publicKey = 'pk_test_gbZv1zKSysff7KprihcWi6ms';
+export const secretKey = 'sk_test_5g4RsDgT1Nm453sk6fFXebZm';
 
 export default {
-	publicKey,
-	secretKey,
+	development: {
+		publicKey,
+		secretKey,
+	},
+	production: {
+		publicKey: '',
+		secretKey: '',
+	},
 };
