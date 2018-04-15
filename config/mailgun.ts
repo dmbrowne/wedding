f702af86a536905e8cbb4fbd91f74dc401f4da37
Daryl Browne <daryl.browne@googlemail.com>
2018-04-15 20:04:26 +0100

    create empy template for mailgun credentials file

---
+++ config/mailgun.ts
@@ -0,0 +1,18 @@
+const developmentSettings = {
+	domain: '',
+	from: '',
+	publicKey: '',
+	apiKey: '',
+};
+
+const productionSettings = {
+	domain: '',
+	from: '',
+	publicKey: '',
+	apiKey: '',
+};
+
+export default {
+	development: developmentSettings,
+	production: productionSettings,
+};
