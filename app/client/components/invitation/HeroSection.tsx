8ec858be8d0a03e0557f4bdb376d7b5ca8823658
Daryl Browne <daryl.browne@googlemail.com>
2018-04-23 22:30:12 +0100

    use stars component insead of flies

--- app/client/components/invitation/HeroSection.tsx
+++ app/client/components/invitation/HeroSection.tsx
@@ -1,12 +1,13 @@
 import './HeroSection.scss';
 import * as React from 'react';
 import FireFlies from '../Fireflies';
+import Stars from '../Stars';
 import InfinityIcon from '../icons/Infinity';
 
 export default function HeroSection(props) {
 	return (
 		<div className="section section-hero">
-			<FireFlies>
+			<Stars>
 				<div className="content">
 					<header>
 						<h1 className="title">The wedding of</h1>
@@ -16,7 +17,7 @@ export default function HeroSection(props) {
 						<h1 className="title">We're getting married!</h1>
 					</header> */}
 				</div>
-			</FireFlies>
+			</Stars>
 		</div>
 	);
 }
