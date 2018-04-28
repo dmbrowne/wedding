ba854cbf82f83240bcd658fd3b2c04072f022f57
Daryl Browne <daryl.browne@googlemail.com>
2018-04-29 00:46:56 +0100

    fix(): use env variable for fetching api keys

--- config/stripeKeys.ts
+++ config/stripeKeys.ts
@@ -1,6 +1,6 @@
 import * as environments from './secrets/stripe.json';
 
-const { publicKey, secretKey } = environments as any;
+const { publicKey, secretKey } = environments[process.env.NODE_ENV || 'development'] as any;
 
 export default {
 	publicKey,
