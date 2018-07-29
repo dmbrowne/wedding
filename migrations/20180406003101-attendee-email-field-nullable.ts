45f7b2bf7332ee3d8d293df75a622fb73e2e295b
Daryl Browne <daryl.browne@googlemail.com>
2018-07-29 02:47:37 +0100

    chore(): migration

--- migrations/20180406003101-attendee-email-field-nullable.ts
+++ migrations/20180406003101-attendee-email-field-nullable.ts
@@ -2,10 +2,6 @@ import Sequelize, { QueryInterface } from 'sequelize';
 
 module.exports = {
 	up: async (queryInterface: QueryInterface) => {
-		await queryInterface.changeColumn('Users', 'email', {
-			type: Sequelize.STRING,
-			allowNull: false,
-		});
 		await queryInterface.changeColumn('Attendees', 'email', {
 			type: Sequelize.STRING,
 			allowNull: true,
@@ -13,10 +9,6 @@ module.exports = {
 	},
 
 	down: async(queryInterface: QueryInterface) => {
-		await queryInterface.changeColumn('Users', 'email', {
-			type: Sequelize.STRING,
-			allowNull: true,
-		});
 		await queryInterface.changeColumn('Attendees', 'email', {
 			type: Sequelize.STRING,
 			allowNull: false,
