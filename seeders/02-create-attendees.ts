e15fdcff458cdf84ea7c8a41729025da99b5a820
Daryl Browne <daryl.browne@googlemail.com>
2020-03-01 05:36:32 +0000

    chore(seeder): remove unused imports

--- seeders/02-create-attendees.ts
+++ seeders/02-create-attendees.ts
@@ -1,35 +1,33 @@
-import models from '../app/server/models';
-import * as faker from 'faker';
-import * as fs from 'fs';
-import * as path from 'path';
+import models from "../app/server/models";
+import * as faker from "faker";
 
 module.exports = {
-	up: () => {
-		const fakeUsers = [];
-		for (let i = 0; i < 50; i++) {
-			fakeUsers.push({
-				email: faker.internet.email(),
-				firstName: faker.name.firstName(),
-				lastName: faker.name.lastName(),
-			});
-		}
+  up: () => {
+    const fakeUsers = [];
+    for (let i = 0; i < 50; i++) {
+      fakeUsers.push({
+        email: faker.internet.email(),
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName()
+      });
+    }
 
-		return models.Attendee.bulkCreate([
-			{
-				email: 'daryl.browne@gmail.com',
-				firstName: 'Darly',
-				lastName: 'Browney',
-			},
-			{
-				email: 'yasminobosi@yahoo.com',
-				firstName: 'Yazzy',
-				lastName: 'Bee',
-			},
-			...fakeUsers,
-		]);
-	},
+    return models.Attendee.bulkCreate([
+      {
+        email: "daryl.browne@gmail.com",
+        firstName: "Darly",
+        lastName: "Browney"
+      },
+      {
+        email: "yasminobosi@yahoo.com",
+        firstName: "Yazzy",
+        lastName: "Bee"
+      },
+      ...fakeUsers
+    ]);
+  },
 
-	down: () => {
-		return true;
-	},
+  down: () => {
+    return true;
+  }
 };
